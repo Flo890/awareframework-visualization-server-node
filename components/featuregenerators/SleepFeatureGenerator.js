1e878d38d76b00590d8eceb3407145257a0468dc
Flo890 <florian.bemmann@gmx.de>
2018-09-11 15:17:13 +0200

    improving Sleep feature

--- components/featuregenerators/SleepFeatureGenerator.js
+++ components/featuregenerators/SleepFeatureGenerator.js
@@ -10,51 +10,60 @@ class SleepFeatureGenerator extends AbstractFeatureGenerator {
             granularityMins = 60*24;
         }
 
-        new FeatureFetcher().getGeneratedFeature('phone_usage', deviceId, from, to, 10, phoneUsages => {
-            // 0. remove 0-usages, sort the phone usages. order is not guaranteed yet
-            phoneUsages = phoneUsages.filter(aUsage => {return aUsage.value > 0});
-            phoneUsages.sort((usage1,usage2) => {return usage1.timestamp-usage2.timestamp;});
+        let featureFetcher = new FeatureFetcher();
 
-            // 1. get first and last phone usage of a day
-            let currentDayTimestamp = 0;
-            let days = {}; // ->  {1234: {firstUsageTimestamp:1235, lastUsageTimestamp: 1240}}, ... }
-            for(let i = 0; i<phoneUsages.length; i++) {
-                let phoneUsagePeriodTimestamp = phoneUsages[i].timestamp;
-                // shift timestamp by 4 hours, so that going to bed up 3:59 a.m. is still recognized
-                let shiftedTimestamp = phoneUsagePeriodTimestamp - (4*60*60*1000);
-                let phoneUsageDayTimestamp = AbstractFeatureGenerator.getBinnedTimestampForGranularity(shiftedTimestamp, 60*24);
-                if (phoneUsageDayTimestamp > currentDayTimestamp) {
-                    // day switch. set the previous phone usage as last of the previous day
-                    if (i>=1) {
-                        let prevPhoneUsage = phoneUsages[i - 1];
+        featureFetcher.getGeneratedFeature('phone_usage', deviceId, from, to, 10, phoneUsages => {
+            featureFetcher.getUsualFeature('linear_accelerometer', deviceId, undefined, from, to, 10, phoneMovement => {
+                let movements = phoneMovement.filter(aMovementData => {return aMovementData.value > 0.05});
+                let mergedUsageData = [];
+                mergedUsageData.push(...phoneUsages);
+                mergedUsageData.push(...movements);
+
+
+                // 0. remove 0-usages, sort the phone usages. order is not guaranteed yet
+                mergedUsageData = mergedUsageData.filter(aUsage => {return aUsage.value > 0});
+                mergedUsageData.sort((usage1,usage2) => {return usage1.timestamp-usage2.timestamp;});
+
+                // 1. get first and last phone usage of a day
+                let currentDayTimestamp = 0;
+                let days = {}; // ->  {1234: {firstUsageTimestamp:1235, lastUsageTimestamp: 1240}}, ... }
+                for(let i = 0; i<mergedUsageData.length; i++) {
+                    let phoneUsagePeriodTimestamp = mergedUsageData[i].timestamp;
+                    // shift timestamp by 4 hours, so that going to bed up 3:59 a.m. is still recognized
+                    let shiftedTimestamp = phoneUsagePeriodTimestamp - (4*60*60*1000);
+                    let phoneUsageDayTimestamp = AbstractFeatureGenerator.getBinnedTimestampForGranularity(shiftedTimestamp, 60*24);
+                    if (phoneUsageDayTimestamp > currentDayTimestamp) {
+                        // day switch. set the previous phone usage as last of the previous day
+                        if (i>=1) {
+                            let prevPhoneUsage = mergedUsageData[i - 1];
+                            if (!days[currentDayTimestamp]) days[currentDayTimestamp] = {};
+                            days[currentDayTimestamp].lastUsageTimestamp = prevPhoneUsage.timestamp + prevPhoneUsage.value * 1000;
+                        }
+
+                        currentDayTimestamp = phoneUsageDayTimestamp;
+
+                        // and this phone usage as the first of this day
                         if (!days[currentDayTimestamp]) days[currentDayTimestamp] = {};
-                        days[currentDayTimestamp].lastUsageTimestamp = prevPhoneUsage.timestamp + prevPhoneUsage.value * 1000;
+                        days[currentDayTimestamp].firstUsageTimestamp = phoneUsagePeriodTimestamp;
+                    }
+                }
+
+                // 2. calculate sleep time for each day (regarding the night before that day)
+                let sleeptimes = [];
+                for(let i =0; i<Object.keys(days).length; i++){
+                    let dayTimestamp = Object.keys(days)[i];
+
+                    if (!days[dayTimestamp-(24*60*60*1000)]){
+                        // previous day has no data
+                        continue;
                     }
 
-                    currentDayTimestamp = phoneUsageDayTimestamp;
-
-                    // and this phone usage as the first of this day
-                    if (!days[currentDayTimestamp]) days[currentDayTimestamp] = {};
-                    days[currentDayTimestamp].firstUsageTimestamp = phoneUsagePeriodTimestamp;
+                    let sleepTimeMillis = days[dayTimestamp].firstUsageTimestamp - days[dayTimestamp-(24*60*60*1000)].lastUsageTimestamp;
+                    sleeptimes.push({timestamp: Number(dayTimestamp), value: sleepTimeMillis/(1000*60*60)});
                 }
-            }
-
-            // 2. calculate sleep time for each day (regarding the night before that day)
-            let sleeptimes = [];
-            for(let i =0; i<Object.keys(days).length; i++){
-                let dayTimestamp = Object.keys(days)[i];
-
-                if (!days[dayTimestamp-(24*60*60*1000)]){
-                    // previous day has no data
-                    continue;
-                }
-
-                let sleepTimeMillis = days[dayTimestamp].firstUsageTimestamp - days[dayTimestamp-(24*60*60*1000)].lastUsageTimestamp;
-                sleeptimes.push({timestamp: dayTimestamp, value: sleepTimeMillis/1000});
-            }
-            callback(sleeptimes);
+                callback(sleeptimes);
+            });
         });
-
     }
 
 }
--- service/FeatureFetcher.js
+++ service/FeatureFetcher.js
@@ -38,7 +38,7 @@ class FeatureFetcher {
                             // TODO another bad architecture thing
                             // RescueTime data is hourly, so we have to upsample it if granularity is higher than 60 minutes
                             if (granularityMins < 60 && sources[i].source_table == 'rescuetime_usage_log') {
-                                this.upsampleData(data,granularityMins);
+                                this.upsampleData(data, 60, granularityMins, (data1,data2) => (data2.value + data1.value) / 2);
                             }
                             // weather: if temperature is in Kelvin, convert it to Celsius
                             if (featureName == 'temperature'){
@@ -61,19 +61,31 @@ class FeatureFetcher {
         let className = this.datamappings.mappings[featureName].feature_generator;
         let DynamicFeatureGeneratorClass = require(`../components/featuregenerators/${className}`);
         let featureGenerator = new DynamicFeatureGeneratorClass(this.dbService);
-        featureGenerator.getData(featureName, deviceId, from, to, granularityMins, dataCb, this.datamappings.mappings[featureName].subfeature);
+        featureGenerator.getData(featureName, deviceId, from, to, granularityMins, data => {
+            if (granularityMins < 60*24 && granularityMins > 0 && featureName == 'sleep') {
+                this.upsampleData(data, 60*24, granularityMins, (data1, data2) => data1.value);
+            }
+            dataCb(data);
+        }, this.datamappings.mappings[featureName].subfeature);
     }
 
-    upsampleData(data, targetGranularityMins){
+    /**
+     * increases the granularity of a result dataset by adding points in a regular interval between the existing ones
+     * @param data something like [{timestamp: 1234, value: 42}, {...}, ...]
+     * @param currentGranularityMins granularity of the dataset given in data property
+     * @param targetGranularityMins target granularity, which the object passed in the data property will have after this call
+     * @param interpolationFn the interpolation function:  data1, data3 => value2    where, each datax object is a {timestamp:1234, value:42} like object, and value2 is only the value!
+     */
+    upsampleData(data, currentGranularityMins, targetGranularityMins, interpolationFn){
         for(let i = 0; i<data.length-1; i++) {
             let timediffToNext = data[i+1].timestamp - data[i].timestamp;
-            if (timediffToNext == 60*60*1000) {
+            if (timediffToNext == currentGranularityMins*60*1000) {
                 // if data granularity is hourly (for RescueTime the maximum) upsample this gap to target granularity
                 let prevRealTimestamp = data[i].timestamp;
                 for(let j = 0; j<timediffToNext/(targetGranularityMins*60*1000); j++) {
                     let newObject = {
-                        timestamp: prevRealTimestamp + (targetGranularityMins*60*1000*(j+1)),
-                        value: (data[i + 1].value + data[i].value) / 2
+                        timestamp: Number(prevRealTimestamp) + (targetGranularityMins*60*1000*(j+1)),
+                        value: interpolationFn(data[i],data[i+1])
                     }
                     data.splice(i+1, 0, newObject);
                     i++;
